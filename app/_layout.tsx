43e8aab01e3291769e390a8a70dd372dcc177c0e
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2025-10-04 18:54:35 +0530

    init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,22 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
+import { useFonts } from "expo-font";
 import { StatusBar } from 'expo-status-bar';
+import * as SplashScreen from "expo-splash-screen";
 import 'react-native-reanimated';
 
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
+SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  });
+
 
   return (
       <>
