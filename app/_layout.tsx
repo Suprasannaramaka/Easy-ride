8f9bebd208909c3d1c3d9645eb0a4b4b3eb9f638
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2025-10-05 23:33:44 +0530

    init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,7 +20,8 @@ export default function RootLayout() {
       <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="+not found"/>
-    </Stack><StatusBar style="auto" />
+    </Stack>
+    <StatusBar style="auto" />
     </>
   );
 }
