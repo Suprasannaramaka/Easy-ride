6220f2b8827bd910849f0d7958976206db4ace74
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2025-10-07 18:44:07 +0530

    submit

---
+++ app/modal.tsx
@@ -0,0 +1,12 @@
+import react from "react";
+import {View , Text } from "react-native";
+ 
+export default function ModelScreen() {
+return(
+    <view>
+        <Text>
+            This is a model screen
+        </Text>
+    </view>
+)
+}
