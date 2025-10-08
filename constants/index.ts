56ddc062596b1a145572221c99fd94375ae2f8c4
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2025-10-08 12:00:55 +0530

    init

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,6 @@
-import onboarding1 from "@/assets/images/onboarding1.png";
-import onboarding2 from "@/assets/images/onboarding2.png";
-import onboarding3 from "@/assets/images/onboarding3.png";
+import onboarding1 from "@/assets/images/onboarding1.jpg";
+import onboarding2 from "@/assets/images/onboarding2.jpg";
+import onboarding3 from "@/assets/images/onboarding3.jpg";
 export const images = {
   onboarding1,
   onboarding2,
